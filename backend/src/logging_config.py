2a8c241fbd12fb69832b6acd2a25587d3ef2449c
AndreFerreira5 <andre.s.ferreira.vip@gmail.com>
2025-10-16 16:39:14 +0100

    initial version of the backend module

---
+++ backend/src/logging_config.py
@@ -0,0 +1,14 @@
+import logging
+
+
+def configure_logging():
+    logging.basicConfig(
+        level=logging.INFO,
+        format="{asctime} [{name}] [{levelname}] {message}",
+        style='{',
+        handlers=[
+            logging.StreamHandler(),
+            logging.FileHandler("app.log")
+        ]
+    )
+    
---
+++ backend/src/main.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI
+import logging
+from logging_config import configure_logging
+
+
+# configure logging globally
+configure_logging()
+logger = logging.getLogger(__name__)
+
+app = FastAPI()
+
+
+@app.get("/ping")
+async def ping():
+    return {"message": "pong"}
+
+
+if __name__ == "__main__":
+    uvicorn.run('main:app', host="0.0.0.0", port=3000, reload=True)
