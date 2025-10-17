55db1f050263a83f47cf08b752ef2499445d5971
AndreFerreira5 <andre.s.ferreira.vip@gmail.com>
2025-10-18 00:23:43 +0100

    fixed files formatting

--- src/logging_config.py
+++ src/logging_config.py
@@ -5,10 +5,6 @@ def configure_logging():
     logging.basicConfig(
         level=logging.INFO,
         format="{asctime} [{name}] [{levelname}] {message}",
-        style='{',
-        handlers=[
-            logging.StreamHandler(),
-            logging.FileHandler("app.log")
-        ]
+        style="{",
+        handlers=[logging.StreamHandler(), logging.FileHandler("app.log")],
     )
-
--- src/test_main.py
+++ src/test_main.py
@@ -4,6 +4,7 @@ from .main import app
 
 client = TestClient(app)
 
+
 def test_ping_pong():
     response = client.get("/ping")
     assert response.status_code == 200
