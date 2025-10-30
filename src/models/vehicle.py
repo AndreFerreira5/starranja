168be4b1fce97c1264b99470c77ff3b54d0caef6
Duarte Neves <duarteneves2003@gmail.com>
2025-10-30 17:33:22 +0000

    models: inital commit with vehicle model as example on how to create a table model; pyproject.toml: added dependencies for the model-table mapping/handling

---
+++ src/models/vehicle.py
@@ -0,0 +1,66 @@
+﻿from datetime import datetime
+from beanie import Document, Indexed
+from pydantic import Field, BaseModel, ConfigDict, field_validator, constr
+from bson import ObjectId
+
+VIN = constr(min_length=17, max_length=17)
+
+
+# - This is the main entity model, as it is defined in the db table, it is used
+# - The (Document) is a base class for MongoDB models,
+#   that means the Beanie will map the class to the collection - this is done in the class Settings below
+class Vehicle(Document):
+    client_id: Indexed(ObjectId) #Indexed means the field will have an Index for faster queries
+    license_plate: Indexed(str, unique=True) = Field(..., alias="licensePlate") # Field is just for us to keep the model properties named correctly with python (snake_case) and still use the table fields original name (camelCase)
+    brand: str
+    model: str
+    kilometers: int = Field(ge=0)
+    vin: Indexed(VIN, unique=True)
+    created_at: datetime = Field(default_factory=datetime.utcnow, alias="createdAt")
+    updated_at: datetime = Field(default_factory=datetime.utcnow, alias="updatedAt")
+
+    model_config = ConfigDict(populate_by_name=True)  # allow snake_case in code, camelCase in payloads
+
+    class Settings:
+        name = "vehicles"  # collection name
+
+    # keep updated_at field fresh
+    async def save(self, *args, **kwargs):
+        self.updated_at = datetime.utcnow()
+        return await super().save(*args, **kwargs)
+
+# ---- Pydantic Schemas (FastAPI I/O) ----
+# this are example uses for the requests, we dont use
+# the Document class for everything so we dont expose internal fields and only expose necessary fields for the requests
+# and only the declared fields can be changed
+
+# - This is the base schema for the vehicles, it is only used for the
+#   others schemas to inherit so we dont have all the fields declared in
+#   all the schemas
+class VehicleBase(BaseModel):
+    license_plate: str = Field(..., alias="licensePlate")
+    brand: str
+    model: str
+    kilometers: int = Field(ge=0)
+    vin: VIN
+
+    model_config = ConfigDict(populate_by_name=True)
+
+# - This is a schema for when creating a vehicle, it inherits the VehicleBase schema
+#   defined above and has client_id because when creating a vehicle we need to attach
+#   it to its owner/client
+class VehicleCreate(VehicleBase):
+    client_id: ObjectId = Field(..., alias="clientId")
+
+class VehicleUpdate(BaseModel):
+    kilometers: int | None = Field(None, ge=0)
+
+    model_config = ConfigDict(populate_by_name=True)
+
+class VehicleOut(VehicleBase):
+    id: str = Field(alias="id")
+    client_id: str = Field(alias="clientId")
+    created_at: datetime = Field(alias="createdAt")
+    updated_at: datetime = Field(alias="updatedAt")
+
+    model_config = ConfigDict(populate_by_name=True, from_attributes=True)
