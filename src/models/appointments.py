a564d33bb58be0682f0c1444908dfe9cdb918633
João Figueiras <joaotomasfigueiras37@gmail.com>
2025-11-08 12:59:47 +0000

    apointment update date removed

--- src/models/appointments.py
+++ src/models/appointments.py
@@ -57,7 +57,6 @@ class AppointmentUpdate(BaseModel):
     vehicle_id: ObjectId | None = Field(None, alias="vehicleId")
     work_order_id: ObjectId | None = Field(None, alias="workOrderId")
 
-    appointment_date: datetime | None = Field(None, alias="appointmentDate")  # is appointment date updatable?
     updated_at: datetime = Field(default_factory=lambda: datetime.now(UTC), alias="updatedAt")
 
     model_config = ConfigDict(populate_by_name=True)
